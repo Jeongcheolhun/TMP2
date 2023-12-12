4c685b441a8b5d1f1304d15d0d12676fa43c0fe4
Jeongcheolhun <hoon2525@gmail.com>
2023-12-12 14:45:22 +0900

    am.

---
+++ 06_/src/main/java/com/example/demo/domain/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.demo.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name="user")
+public class User {
+    @Id
+    private String username;
+    private String password;
+    private String role;
+}
